128658bf5f6d09dfc32be60624ad18e03b24de75
ss <rkdtlstjd123@naver.com>
2023-11-18 21:49:30 +0900

    [Feat] Server upload

---
+++ Spring/spring3.0-gradle-study/src/main/java/com/example/spring3/gradlestudy/domain/DefaultController.java
@@ -0,0 +1,10 @@
+package com.example.spring3.gradlestudy.domain;
+
+public class DefaultController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Spring/spring3.0-gradle-study/src/main/java/com/example/spring3/gradlestudy/domain/post/PostController.java
@@ -0,0 +1,19 @@
+package com.example.spring3.gradlestudy.domain.post;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/post")
+@RequiredArgsConstructor
+public class PostController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
